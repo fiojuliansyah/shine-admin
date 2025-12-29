2066979f080432a52a74ca824f46de0396261191
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-12-29 17:53:07 +0700

    Update SiteAttendanceExport.php

--- app/Exports/SiteAttendanceExport.php
+++ app/Exports/SiteAttendanceExport.php
@@ -18,6 +18,8 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
     protected $highlightCellsLeave = [];
     protected $highlightCellsAlpha = [];
     protected $highlightCellsShiftOff = [];
+    protected $highlightCellsBeritaAcara = [];
+    protected $highlightCellsOvertime = [];
     protected $highlightCellsLate = [];
     protected $totalShiftOffByUser = [];
 
@@ -35,33 +37,30 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
         foreach ($this->attendancesByUser as $user_id => $userAttendances) {
             $user = $userAttendances->first()->user;
             $totals = $this->totalsByUser[$user_id] ?? [
-                'totalHK' => 0,
-                'totalOvertime' => 0,
-                'totalLate' => 0,
-                'totalBA' => 0,
-                'totalLeave' => 0,
+                'totalHK' => '',
+                'totalOvertime' => '',
+                'totalLate' => '',
+                'totalBA' => '',
+                'totalLeave' => '',
             ];
     
             $totalShiftOff = 0;
-    
             $row = [
                 $user->name,
                 $user->employee_nik,
                 $user->roles->first()->name ?? '-', 
             ];
-            
             $rowIndex = count($data) + 3;
     
-            foreach ($this->dates as $date) {
+            foreach ($this->dates as $index => $date) {
                 $attendance = $userAttendances->get($date->format('Y-m-d'));
-                
                 if ($attendance) {
                     if ($attendance->type == 'late') {
                         $this->highlightCellsLate[] = [$rowIndex, count($row)];
                     }
 
                     if ($attendance->leave_id != null) {
-                        $row[] = $attendance->leave->type['name'] ?? 'LEAVE';
+                        $row[] = $attendance->leave->type['name'] ?? '';
                         $row[] = '';
                         $row[] = '';
                         $this->highlightCellsLeave[] = [$rowIndex, count($row) - 3];
@@ -71,6 +70,26 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
                         $row[] = '';
                         $this->highlightCellsShiftOff[] = [$rowIndex, count($row) - 3];
                         $totalShiftOff++;
+                    } elseif ($attendance->type == 'minutes') {
+                        $row[] = $attendance->clock_in ? $attendance->clock_in->format('H:i') : '-';
+                        $row[] = $attendance->clock_out ? $attendance->clock_out->format('H:i') : '-';
+                        $this->highlightCellsBeritaAcara[] = [$rowIndex, count($row) - 2];
+    
+                        $totalOvertimeMinutes = 0;
+                        foreach ($attendance->overtimes as $overtime) {
+                            try {
+                                $overtimeStart = Carbon::parse($overtime->clock_in);
+                                $overtimeEnd = Carbon::parse($overtime->clock_out);
+                                if ($overtimeEnd && $overtimeStart) {
+                                    $totalOvertimeMinutes += $overtimeStart->diffInMinutes($overtimeEnd);
+                                }
+                            } catch (\Exception $e) {}
+                        }
+                        $overtimeHours = intdiv($totalOvertimeMinutes, 60);
+                        $remainingMinutes = $totalOvertimeMinutes % 60;
+                        $overtimeFormatted = $totalOvertimeMinutes > 0 ? sprintf('%02d:%02d', $overtimeHours, $remainingMinutes) : '-';
+                        $row[] = $overtimeFormatted;
+                        $this->highlightCellsOvertime[] = [$rowIndex, count($row) - 1];
                     } else {
                         $row[] = $attendance->clock_in ? $attendance->clock_in->format('H:i') : '-';
                         $row[] = $attendance->clock_out ? $attendance->clock_out->format('H:i') : '-';
@@ -85,10 +104,11 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
                                 }
                             } catch (\Exception $e) {}
                         }
-                        
                         $overtimeHours = intdiv($totalOvertimeMinutes, 60);
                         $remainingMinutes = $totalOvertimeMinutes % 60;
-                        $row[] = $totalOvertimeMinutes > 0 ? sprintf('%02d:%02d', $overtimeHours, $remainingMinutes) : '-';
+                        $overtimeFormatted = $totalOvertimeMinutes > 0 ? sprintf('%02d:%02d', $overtimeHours, $remainingMinutes) : '-';
+                        $row[] = $overtimeFormatted;
+                        $this->highlightCellsOvertime[] = [$rowIndex, count($row) - 1];
                     }
                 } else {
                     $row[] = '';
@@ -123,12 +143,7 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
         }
     
         $headings = array_merge($headings, [
-            'Total HK',
-            'Total Lembur',
-            'Total Telat',
-            'Total BA',
-            'Total Cuti',
-            'Total OFF',
+            'Total HK', 'Total Lembur', 'Total Telat', 'Total BA', 'Total Cuti', 'Total OFF',
         ]);
     
         $subHeadings = ['', '', ''];
@@ -146,14 +161,11 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
     
     public function styles(Worksheet $sheet)
     {
-        $lastColumn = $sheet->getHighestColumn();
-        $highestRow = $sheet->getHighestRow();
-
         $sheet->getStyle('1:2')->getFont()->setName('Verdana')->setSize(14)->setBold(true);
         $sheet->getStyle('1:2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER)->setVertical(Alignment::VERTICAL_CENTER);
     
         $startColumnIndex = 4;
-        foreach ($this->dates as $date) {
+        foreach ($this->dates as $index => $date) {
             $endColumnIndex = $startColumnIndex + 2;
             $sheet->mergeCells($this->getColumnLetter($startColumnIndex) . '1:' . $this->getColumnLetter($endColumnIndex) . '1');
             $startColumnIndex = $endColumnIndex + 1;
@@ -162,33 +174,48 @@ class SiteAttendanceExport implements FromCollection, WithHeadings, WithStyles
         $sheet->getRowDimension(1)->setRowHeight(25);
         $sheet->getRowDimension(2)->setRowHeight(25);
     
-        foreach (range(3, $highestRow) as $rowIndex) {
-            $sheet->getRowDimension($rowIndex)->setRowHeight(30);
+        foreach (range(3, $sheet->getHighestRow()) as $rowIndex) {
+            $sheet->getRowDimension($rowIndex)->setRowHeight(100);
+            $lastColumn = $sheet->getHighestColumn(); 
             $sheet->getStyle('A' . $rowIndex . ':' . $lastColumn . $rowIndex)
-                ->getFont()->setName('Verdana')->setSize(12);
+                ->getFont()->setName('Verdana')->setSize(14)->setBold(true);  
         }
         
-        $sheet->getStyle('A1:' . $lastColumn . $highestRow)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER)->setVertical(Alignment::VERTICAL_CENTER);
-        $sheet->getDefaultColumnDimension()->setWidth(12);
+        $lastColumn = $sheet->getHighestColumn(); 
+        $sheet->getStyle('A1:' . $lastColumn . $sheet->getHighestRow())->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER)->setVertical(Alignment::VERTICAL_CENTER);
+        $sheet->getDefaultColumnDimension()->setWidth(15);
     
         foreach ($this->highlightCellsLeave as [$rowIndex, $columnIndex]) {
-            $range = $this->getColumnLetter($columnIndex + 1) . $rowIndex . ':' . $this->getColumnLetter($columnIndex + 3) . $rowIndex;
-            $sheet->getStyle($range)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('00B0F0');
+            $startCell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
+            $endCell = $this->getColumnLetter($columnIndex + 3) . $rowIndex;
+            $sheet->getStyle("$startCell:$endCell")->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('00B0F0');
         }
     
         foreach ($this->highlightCellsAlpha as [$rowIndex, $columnIndex]) {
-            $range = $this->getColumnLetter($columnIndex + 1) . $rowIndex . ':' . $this->getColumnLetter($columnIndex + 3) . $rowIndex;
-            $sheet->getStyle($range)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('FF0000');
+            $startCell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
+            $endCell = $this->getColumnLetter($columnIndex + 3) . $rowIndex;
+            $sheet->getStyle("$startCell:$endCell")->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('FF0000');
         }
     
         foreach ($this->highlightCellsShiftOff as [$rowIndex, $columnIndex]) {
-            $range = $this->getColumnLetter($columnIndex + 1) . $rowIndex . ':' . $this->getColumnLetter($columnIndex + 3) . $rowIndex;
-            $sheet->getStyle($range)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('92D050');
+            $startCell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
+            $endCell = $this->getColumnLetter($columnIndex + 3) . $rowIndex;
+            $sheet->getStyle("$startCell:$endCell")->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('92D050');
         }
 
         foreach ($this->highlightCellsLate as [$rowIndex, $columnIndex]) {
             $cell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
             $sheet->getStyle($cell)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('FFFF00');
+        }
+
+        foreach ($this->highlightCellsBeritaAcara as [$rowIndex, $columnIndex]) {
+            $cell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
+            $sheet->getStyle($cell)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('E26B0A');
+        }
+
+        foreach ($this->highlightCellsOvertime as [$rowIndex, $columnIndex]) {
+            $cell = $this->getColumnLetter($columnIndex + 1) . $rowIndex;
+            $sheet->getStyle($cell)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('FFC000');
         }
     }
     
