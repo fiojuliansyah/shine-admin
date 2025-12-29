66871a1bd35edd5107dd051b55e6758093d4d770
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-12-29 12:04:28 +0700

    update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,7 +8,7 @@
 	<meta name="keywords" content="admin, estimates, bootstrap, business, html5, responsive, Projects">
 	<meta name="author" content="Dreams technologies - Bootstrap Admin Template">
 	<meta name="robots" content="noindex, nofollow">
-	<title>GIS - HRIS & Productivity Management</title>
+	<title>SHINE - HRIS & Productivity Management</title>
 
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="/admin/assets/img/favicon.png">
--- resources/views/website/layouts/app.blade.php
+++ resources/views/website/layouts/app.blade.php
@@ -8,7 +8,7 @@
 	<meta name="keywords" content="admin, estimates, bootstrap, business, html5, responsive, Projects">
 	<meta name="author" content="Dreams technologies - Bootstrap Admin Template">
 	<meta name="robots" content="noindex, nofollow">
-	<title>GIS - Job Portal</title>
+	<title>SHINE - Job Portal</title>
 
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="/admin/assets/img/favicon.png">
