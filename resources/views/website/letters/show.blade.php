99d8aaab1ba458ebdd008b916671a97dac88216b
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-12-30 14:00:22 +0700

    Update show.blade.php

--- resources/views/website/letters/show.blade.php
+++ resources/views/website/letters/show.blade.php
@@ -4,61 +4,67 @@
 <div class="page-wrapper" style="background: #eceef4; min-height: 100vh; padding-bottom: 3rem;">
     <div class="content">
 
-        <div class="card mx-auto shadow-lg border-0" style="max-width: 850px;">
-            <div class="card-body p-0">
-                <div id="printableArea" style="
-                    background-color: #fff; 
-                    padding: 5rem 4rem; 
-                    min-height: 297mm; 
-                    font-family: 'Times New Roman', Times, serif; 
-                    color: #000; 
-                    line-height: 1.6;
-                    font-size: 12pt;
-                    position: relative;
-                ">
-                    
-                    @if($eletter->esign)
-                        <div style="position: absolute; top: 20%; left: 50%; transform: translate(-50%, -50%) rotate(-45deg); font-size: 5rem; color: rgba(40, 167, 69, 0.1); font-weight: bold; pointer-events: none; text-transform: uppercase; border: 10px solid rgba(40, 167, 69, 0.1); padding: 10px; z-index: 1;">
-                            SIGNED
-                        </div>
-                    @endif
-                    <div class="letter-content">
-                        {!! $eletter->letter->description !!}
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <button type="button" class="btn btn-primary btn-lg rounded-circle shadow-lg" 
-            style="position: fixed; bottom: 30px; right: 30px; width: 60px; height: 60px; z-index: 1050;"
-            data-bs-toggle="modal" data-bs-target="#modalSignature">
-            <i class="ti ti-signature fs-24"></i>
-        </button>
-
-        <div class="modal fade" id="modalSignature" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Tanda Tangan Digital</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form action="{{ route('web.applicants.letter.sign', $eletter->id) }}" method="POST">
-                        @csrf
-                        <div class="modal-body text-center">
-                            <p class="text-muted fs-12">Gunakan mouse atau jari Anda untuk menandatangani dokumen ini.</p>
-                            <div class="border rounded bg-light">
-                                <canvas id="signatureCanvas" style="width: 100%; height: 200px; cursor: crosshair;"></canvas>
+        @if(!$eletter->second_party_esign)
+            <div class="card mx-auto shadow-lg border-0" style="max-width: 850px;">
+                <div class="card-body p-0">
+                    <div id="printableArea" style="
+                        background-color: #fff; 
+                        padding: 5rem 4rem; 
+                        min-height: 297mm; 
+                        font-family: 'Times New Roman', Times, serif; 
+                        color: #000; 
+                        line-height: 1.6;
+                        font-size: 12pt;
+                        position: relative;
+                    ">
+                        
+                        @if($eletter->esign)
+                            <div style="position: absolute; top: 20%; left: 50%; transform: translate(-50%, -50%) rotate(-45deg); font-size: 5rem; color: rgba(40, 167, 69, 0.1); font-weight: bold; pointer-events: none; text-transform: uppercase; border: 10px solid rgba(40, 167, 69, 0.1); padding: 10px; z-index: 1;">
+                                SIGNED
                             </div>
-                            <input type="hidden" name="second_party_esign" id="signatureInput">
+                        @endif
+                        <div class="letter-content">
+                            {!! $eletter->letter->description !!}
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" id="resetSignature" class="btn btn-light">Hapus</button>
-                            <button type="submit" id="saveSignature" class="btn btn-primary">Simpan Tanda Tangan</button>
-                        </div>
-                    </form>
+
+                    </div>
                 </div>
             </div>
-        </div>
+            <button type="button" class="btn btn-primary btn-lg rounded-circle shadow-lg" 
+                style="position: fixed; bottom: 30px; right: 30px; width: 60px; height: 60px; z-index: 1050;"
+                data-bs-toggle="modal" data-bs-target="#modalSignature">
+                <i class="ti ti-signature fs-24"></i>
+            </button>
+
+            <div class="modal fade" id="modalSignature" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Tanda Tangan Digital</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="{{ route('web.applicants.letter.sign', $eletter->id) }}" method="POST">
+                            @csrf
+                            <div class="modal-body text-center">
+                                <p class="text-muted fs-12">Gunakan mouse atau jari Anda untuk menandatangani dokumen ini.</p>
+                                <div class="border rounded bg-light">
+                                    <canvas id="signatureCanvas" style="width: 100%; height: 200px; cursor: crosshair;"></canvas>
+                                </div>
+                                <input type="hidden" name="second_party_esign" id="signatureInput">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" id="resetSignature" class="btn btn-light">Hapus</button>
+                                <button type="submit" id="saveSignature" class="btn btn-primary">Simpan Tanda Tangan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="alert alert-success d-print-none" style="position: fixed; bottom: 20px; right: 20px; z-index: 1050;">
+                <i class="ti ti-check me-1"></i> Dokumen ini telah ditandatangani.
+            </div>
+        @endif
     </div>
 </div>
 @endsection
