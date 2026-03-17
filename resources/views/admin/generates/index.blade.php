b93c9519644705afde2466ad02ec5f068fd70f90
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2026-03-17 15:27:19 +0700

    Update index.blade.php

--- resources/views/admin/generates/index.blade.php
+++ resources/views/admin/generates/index.blade.php
@@ -6,14 +6,14 @@
             <!-- Breadcrumb -->
             <div class="d-md-flex d-block align-items-center justify-content-between page-breadcrumb mb-3">
                 <div class="my-auto mb-2">
-                    <h2 class="mb-1">Generate Surat</h2>
+                    <h2 class="mb-1">Surat Terbit</h2>
                     <nav>
                         <ol class="breadcrumb mb-0">
                             <li class="breadcrumb-item">
                                 <a href="index.html"><i class="ti ti-smart-home"></i></a>
                             </li>
                             <li class="breadcrumb-item">HR</li>
-                            <li class="breadcrumb-item active" aria-current="page">Generate Surat</li>
+                            <li class="breadcrumb-item active" aria-current="page">Surat Terbit</li>
                         </ol>
                     </nav>
                 </div>
